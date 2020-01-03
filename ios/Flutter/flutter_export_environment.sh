1c162703b74cf5235be62bc77e8de3d3363330dc
yengcomet <yengmoua@comet.la>
2020-01-03 15:09:31 +0700

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aphommasone/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aphommasone/Documents/flutter_web"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/aphommasone/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"

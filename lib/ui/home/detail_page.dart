2863140184fab53eb3be2f627cdb09c5559164cb
yengcomet <yengmoua@comet.la>
2020-01-18 23:10:12 +0700

    Fix page detail

---
+++ lib/ui/home/detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web/ui/ui_constractor.dart';
+
+class DetailsPage extends StatefulWidget {
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie Details'),
+        backgroundColor: appBarColor,
+        centerTitle: true,
+      ),
+      body: Center(child: Text("Hello Yeng", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),),),
+    );
+  }
+}
--- lib/ui/home/my_home.dart
+++ lib/ui/home/my_home.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_web/ui/home/detail_page.dart';
 import 'package:flutter_web/ui/ui_constractor.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,60 +24,64 @@ class _MYHomePageState extends State<MYHomePage> {
         centerTitle: true,
         backgroundColor: appBarColor,
       ),
-      body: FutureBuilder(
-        future: getMovies(),
-        builder: (context, snapshot) {
-          Map data = snapshot.data;
-          if (snapshot.hasError) {
-            print("Your Erro ${snapshot.error}");
-            return Text(
-              "Failed to get respone from the server",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 22.0,
-              ),
-            );
-          } else if (snapshot.hasData) {
-            return GridView.builder(
-                gridDelegate:
-                new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.all(10.0),
-                    height: 800,
-                    width: 400,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                          child: InkWell(
-                            onTap: () {},
-                            child: Image.network(
-                                '${data['showing'][index]['poster']}'),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('${data['showing'][index]['title']}',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text('${data['coming'][index]['section_lo']}',style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
-                      ],
-                    ),
-                  );
-                },
+      body: Container(
+        padding: EdgeInsets.only(top: 10, left: 5,right: 5, bottom: 20),
+        child: FutureBuilder(
+          future: getMovies(),
+          builder: (context, snapshot) {
+            Map data = snapshot.data;
+            if (snapshot.hasError) {
+              print("Your Erro ${snapshot.error}");
+              return Text(
+                "Failed to get respone from the server",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 22.0,
+                ),
               );
-          } else if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(
-                backgroundColor: appBarColor,
-              ),
-            );
-          }
-        },
+            } else if (snapshot.hasData) {
+              return GridView.builder(
+                  gridDelegate:
+                  new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: appBarColor),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailsPage()));
+                              },
+                              child: Image.network(
+                                  '${data['showing'][index]['poster']}'),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('${data['showing'][index]['title']}',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('${data['coming'][index]['section_lo']}',style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                    );
+                  },
+                );
+            } else if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: appBarColor,
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }

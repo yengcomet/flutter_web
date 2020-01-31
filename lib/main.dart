9bf5e9bfed6cf6ae20e55970ccd3aabd28b3f0c7
yengcomet <yengmoua@comet.la>
2020-01-31 11:20:35 +0700

    Add get video

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web/ui/detail/detail_page.dart';
 import 'package:flutter_web/ui/home/home_page.dart';
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_web/service/api.dart';
 import 'package:flutter_web/ui/ui_contractor.dart';
 import 'package:flutter_web/widget/error_widget.dart';
 import 'package:video_player/video_player.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class DetailsPage extends StatefulWidget {
   final String title;
@@ -18,26 +19,36 @@ class _DetailsPageState extends State<DetailsPage> {
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
   MovieAPI api = MovieAPI();
+  YoutubePlayerController _controllerY;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-    );
-    _initializeVideoPlayerFuture = _controller.initialize();
+    // _controller = VideoPlayerController.network(
+    //   'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    // );
+    // _initializeVideoPlayerFuture = _controller.initialize();
 
-    _controller.setLooping(true);
+    // _controller.setLooping(true);
+
+     _controllerY = YoutubePlayerController(
+        initialVideoId: 'LFfcUIFkbLg',
+        flags: YoutubePlayerFlags(
+            mute: false,
+            autoPlay: true,
+            forceHideAnnotation: true,
+        ),
+    );    
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    _controllerY.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Movie Details'),
@@ -98,40 +109,52 @@ class _DetailsPageState extends State<DetailsPage> {
                 ),
               ),
               Divider(),
-              FutureBuilder(
-                future: _initializeVideoPlayerFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return AspectRatio(
-                      aspectRatio: _controller.value.aspectRatio,
-                      child: VideoPlayer(_controller),
-                    );
-                  } else {
-                    return Center(child: CircularProgressIndicator(backgroundColor: appBarColor,));
-                  }
+              YoutubePlayer(
+                controller: _controllerY,
+                showVideoProgressIndicator: true,
+                // videoProgressIndicatorColor: Colors.amber,
+                // progressColors: ProgressColors(
+                //     playedColor: Colors.amber,
+                //     handleColor: Colors.amberAccent,
+                // ),
+                onReady: () {
+                    print('Player is ready.');
                 },
               ),
+              // FutureBuilder(
+              //   future: _initializeVideoPlayerFuture,
+              //   builder: (context, snapshot) {
+              //     if (snapshot.connectionState == ConnectionState.done) {
+              //       return AspectRatio(
+              //         aspectRatio: _controller.value.aspectRatio,
+              //         child: VideoPlayer(_controller),
+              //       );
+              //     } else {
+              //       return Center(child: CircularProgressIndicator(backgroundColor: appBarColor,));
+              //     }
+              //   },
+              // ),
             ],
           );
             }
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: appBarColor,
-        onPressed: () {
-          setState(() {
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              _controller.play();
-            }
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ), 
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: appBarColor,
+      //   onPressed: () {
+      //     setState(() {
+      //       if (_controller.value.isPlaying) {
+      //         _controller.pause();
+      //       } else {
+      //         _controller.play();
+      //       }
+      //     });
+      //   },
+      //   child: Icon(
+      //     _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+      //   ),
+      // ), 
     );
   }
 }

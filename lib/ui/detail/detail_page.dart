f5a9b1481bb4f3d31d216083a7ad2c3c14259eac
yengcomet <yengmoua@comet.la>
2020-01-31 08:48:07 +0700

    Fix image path parameter

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_web/service/api.dart';
 import 'package:flutter_web/ui/ui_contractor.dart';
+import 'package:flutter_web/widget/error_widget.dart';
 import 'package:video_player/video_player.dart';
 
 class DetailsPage extends StatefulWidget {
@@ -15,6 +17,7 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
+  MovieAPI api = MovieAPI();
 
   @override
   void initState() {
@@ -42,66 +45,76 @@ class _DetailsPageState extends State<DetailsPage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white),
-                            // child: widget.imagePath,
-                      ),
-                      SizedBox(
-                        width: 100,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            widget.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            widget.description,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+        child: FutureBuilder(
+          future: api.getMovies(),
+          builder: (context, snapshot){
+            Map data = snapshot.data;
+            if(snapshot.hasError){
+              return ErrorWidgetPage();
+            }else if(snapshot.hasData){
+              return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          height: 200,
+                          width: 200,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white),
+                              child: Image.network('${widget.imagePath}'),
+                        ),
+                        SizedBox(
+                          width: 100,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              widget.title,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              widget.description,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Divider(),
-            FutureBuilder(
-              future: _initializeVideoPlayerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  );
-                } else {
-                  return Center(child: CircularProgressIndicator(backgroundColor: appBarColor,));
-                }
-              },
-            ),
-          ],
+              Divider(),
+              FutureBuilder(
+                future: _initializeVideoPlayerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator(backgroundColor: appBarColor,));
+                  }
+                },
+              ),
+            ],
+          );
+            }
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
